740fd7c20555fa38785b599f01a4d5627325117c
iam-harshit <importantemail545@gmail.com>
2024-07-03 23:24:12 +0530

    Make vercel.json file and configure index.js file for serverless function

--- backend/index.js
+++ backend/index.js
@@ -32,10 +32,7 @@ app.use("/transaction", transactionRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
@@ -46,3 +43,5 @@ mongoose
     // Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
+
+export default app;
