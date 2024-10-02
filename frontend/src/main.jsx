898e18324c389f4f87e6826bf9479e68b4c1e9c0
iam-harshit <importantemail545@gmail.com>
2024-10-03 02:02:40 +0530

    Fix runtime error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "@/state/api";
-import * as serviceWorkerRegistration from "../serviceWorkerRegistration";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 export const store = configureStore({
   reducer: { [api.reducerPath]: api.reducer },
